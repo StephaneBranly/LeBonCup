f86f76e864e7a84f309ae6bc58b3d5c6b33639f0
StephaneBranly <stephanebranly.pro@gmail.com>
2020-04-30 21:38:42 +0200

    fix: improve component

--- components/research_component.php
+++ components/research_component.php
@@ -2,20 +2,16 @@
     function research_component() 
     {
         echo"<section id='research_component'>
-        <table>
-            <tr><td>Partie une</td>
-            <td></td>
-            <td>
             <div id='research'><input placeholder='Rechercher' id='input_research' type='text'/></div>
             <div id='category'><i class='icon-menu'></i><span id='label_category'>Toutes catégories</span><i class='icon-down-open'></i></div>
-            <p>
-  <label for='amount'>Price range:</label>
-  <input type='text' id='amount' readonly style='border:0; color:#f6931f; font-weight:bold;'>
-</p>
- 
-<div id='slider-range'></div>
-            </td></tr>
-        </table>
-        </section>";
+            <div id='price_range'>
+                <p>
+                <label for='amount'>Price range:</label>
+                <input type='text' id='amount' readonly style='border:0; color:#f6931f; font-weight:bold;'>
+                </p>
+                <div id='slider-range'></div>
+            </div>
+        </section>
+        <section id='research_button_section'><div id='research_button'>Rechercher <i class='icon-search'></i></div></section>";
     }
 ?>
--- components/services/research_component.js
+++ components/services/research_component.js
@@ -5,7 +5,7 @@ $(function() {
     max: 500,
     values: [75, 300],
     slide: function(event, ui) {
-      $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+      $("#amount").val(ui.values[0]+"€ - " + ui.values[1]+"€");
     }
   });
   $("#amount").val(
