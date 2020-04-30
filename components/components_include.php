c0dcb5225a1313bfbbdb5f10c9946d29a3ace28f
StephaneBranly <stephanebranly.pro@gmail.com>
2020-04-30 20:55:44 +0200

    add: first filters to research

--- components/components_include.php
+++ components/components_include.php
@@ -1,3 +1,7 @@
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
 <link href="../ressources/fontello/css/fontello.css" rel="stylesheet" media="all" type="text/css">
 
@@ -14,3 +18,4 @@
 ?>
 
 <script type="text/javascript" src="../components/services/header.js"></script>
+<script type="text/javascript" src="../components/services/research_component.js"></script>
--- components/research_component.php
+++ components/research_component.php
@@ -6,8 +6,14 @@
             <tr><td>Partie une</td>
             <td></td>
             <td>
-            <div id='research'><input placeholder='Rechercher' id='input_research' type='text'/><i class='icon-search'></i></div>
+            <div id='research'><input placeholder='Rechercher' id='input_research' type='text'/></div>
             <div id='category'><i class='icon-menu'></i><span id='label_category'>Toutes catégories</span><i class='icon-down-open'></i></div>
+            <p>
+  <label for='amount'>Price range:</label>
+  <input type='text' id='amount' readonly style='border:0; color:#f6931f; font-weight:bold;'>
+</p>
+ 
+<div id='slider-range'></div>
             </td></tr>
         </table>
         </section>";
