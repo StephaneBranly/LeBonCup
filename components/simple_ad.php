b0e71cc67d72c5602ea87aca8645b6201a1ce4fa
StephaneBranly <stephanebranly.pro@gmail.com>
2020-05-01 02:08:38 +0200

    fix: improve simple_ad

--- components/simple_ad.php
+++ components/simple_ad.php
@@ -1,22 +1,25 @@
 <?php
     function simple_ad() 
     {
-        echo "<section class='simple_ad'>
+        echo "<a href='annonce-numero' target='_blank'><section class='simple_ad'>
         <table>
             <tr>
                 <td class='left' style=\"background-image: url('http://planetepetitsloups.com/images/produits/voitures-a-pedales-tracteur-remorque-pelle-excavatrice.jpg');\">
                     <span class='nb_photos'>2<i class='icon-camera'></i></span>
                 </td>
                 <td class='center'>
-                    <h1>Title annonce</h1>
+                    <h1><span class='price'><i class='icon-tag'></i>70€</span>Title annonce</h1>
                     <p>Description annonce tronquée à la fin</p>
                     <div class='details'>
-                        <span class='seller'>posté par branlyst</span><span class='date_post'>30/04/2020 22:43</span><span class='views'>56<i class='icon-eye'></i></div>
+                        <a href='user/branlyst' target='_brank'><span class='seller'>posté par branlyst</span></a>
+                        <span class='date_post'>30/04/2020 22:43</span>
+                        <span class='views'>56<i class='icon-eye'></i></span>
+                        <span class='likes'>3<i class='icon-heart'></i></span></div>
                     </div>
                 </td>
             </tr>
         </table>
-        </section>
+        </section></a>
         ";
     }
 ?>
