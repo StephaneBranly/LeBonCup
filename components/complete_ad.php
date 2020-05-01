ee00e7b04e41aed4138048c166cfc1f08e265f22
StephaneBranly <stephanebranly.pro@gmail.com>
2020-05-01 15:58:00 +0200

    add: enlarge photo viewed

--- components/complete_ad.php
+++ components/complete_ad.php
@@ -1,9 +1,14 @@
 <?php
     function complete_ad() 
     {
-        echo "<section class='complete_ad'>
+        echo "<section id='complete_ad'>
         <h1><span class='price'><i class='icon-tag'></i>70€</span>Title annonce</h1>
-        <div id='photo' style=\"background-image: url('http://planetepetitsloups.com/images/produits/voitures-a-pedales-tracteur-remorque-pelle-excavatrice.jpg');\"></div>
+        <div id='photo' style=\"background-image: url('http://planetepetitsloups.com/images/produits/voitures-a-pedales-tracteur-remorque-pelle-excavatrice.jpg');\">
+            <span id='enlarge' onclick='enlarge_photo();'><i class='icon-resize-full'></i></span>
+        </div>
+        <div id='viewer_enlarge' style='display:none;'>
+            <span id='back' onclick='back_photo();'><i class='icon-resize-small'></i></span>
+        </div>
         <div id='photos'>
             <div onclick=\"change_photo('http://planetepetitsloups.com/images/produits/voitures-a-pedales-tracteur-remorque-pelle-excavatrice.jpg');\" class='miniature_photo' style=\"background-image: url('http://planetepetitsloups.com/images/produits/voitures-a-pedales-tracteur-remorque-pelle-excavatrice.jpg');\"></div>
             <div onclick=\"change_photo('https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Lippincott_doriangray.jpg/1200px-Lippincott_doriangray.jpg')\" class='miniature_photo' style=\"background-image: url('https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Lippincott_doriangray.jpg/1200px-Lippincott_doriangray.jpg');\"></div>
