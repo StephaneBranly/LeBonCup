67559c822d8314c6e22d3e92054d12f8a15f9303
StephaneBranly <stephanebranly.pro@gmail.com>
2020-05-01 16:04:39 +0200

    add: working search input

--- components/services/header.js
+++ components/services/header.js
@@ -1,10 +1,6 @@
-function search_sthg(this_) {
-  var text_search = document.getElementById("text_search");
-  var input_search = document.getElementById("input_search");
-
-  if (text_search.style.display != "none") {
-    text_search.style.display = "none";
-    input_search.style.display = "inline";
-    input_search.focus();
-  }
+function search_sthg() {
+  var input_search = document.getElementById("input_search").value;
+  input_search.replace(new RegExp(" ", "g"), "-").toLowerCase();
+  url = "../search/toutes-categories/" + input_search;
+  document.location.href = url;
 }
