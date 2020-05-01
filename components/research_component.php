88cfa80fb35af623bd3a5a3beddf747c5df27db4
StephaneBranly <stephanebranly.pro@gmail.com>
2020-05-01 02:40:21 +0200

    add: select category

--- components/research_component.php
+++ components/research_component.php
@@ -3,7 +3,34 @@
     {
         echo"<section id='research_component'>
             <div id='research'><input placeholder='Rechercher' id='input_research' type='text'/></div>
-            <div id='category'><i class='icon-menu'></i><span id='label_category'>Toutes catégories</span><i class='icon-down-open'></i></div>
+            <div id='category' onclick='show_categories(this);'><i class='icon-menu'></i><span id='label_category'>Toutes catégories</span><i class='icon-down-open'></i></div>
+            <table id='categories' style='display:none'>
+                <tr>
+                <td>
+                    <span class='a_category' onclick=\"change_category('Toutes catégories');\"><i class='icon-menu'></i>Toutes catégories</span>
+                    <span class='a_category' onclick=\"change_category('Logement');\"><i class='icon-lodging'></i>Logement</span>
+                    <span class='a_subcategory' onclick=\"change_category('Location');\">Location</span>
+                    <span class='a_subcategory' onclick=\"change_category('Colocation');\">Colocation</span>
+                    <span class='a_subcategory' onclick=\"change_category('Chambre chez l\'habitant');\">Chambre chez l'habitant</span>
+                    <span class='a_subcategory' onclick=\"change_category('Mobilier');\">Mobilier</span>
+                    <span class='a_subcategory' onclick=\"change_category('Décoration');\">Décoration</span>
+                    <span class='a_subcategory' onclick=\"change_category('Vaisselle');\">Vaisselle</span>
+                </td>
+                <td>
+                    <span class='a_category' onclick=\"change_category('Transport');\" ><i class='icon-bicycle'></i>Transport</span>
+                    <span class='a_subcategory' onclick=\"change_category('Voiture');\" >Voiture</span>
+                    <span class='a_subcategory'onclick=\"change_category('Vélo');\" >Vélo</span>
+                    <span class='a_subcategory' onclick=\"change_category('Skate');\">Skate</span>
+                </td>
+                <td>
+                    <span class='a_category' onclick=\"change_category('Etudes');\" ><i class='icon-graduation-cap'></i>Etudes</span>
+                    <span class='a_subcategory' onclick=\"change_category('Cours');\" >Cours</span>
+                    <span class='a_subcategory' onclick=\"change_category('Polycopiés');\">Polycopiés</span>
+                    <span class='a_subcategory' onclick=\"change_category('Annales');\" >Annales</span>
+                    <span class='a_subcategory' onclick=\"change_category('Fournitures');\" >Fournitures</span>
+                </td>
+                </td>
+            </table>
             <div id='price_range'>
                 <p>
                 <label for='amount'>Price range:</label>
--- components/services/research_component.js
+++ components/services/research_component.js
@@ -5,7 +5,7 @@ $(function() {
     max: 500,
     values: [75, 300],
     slide: function(event, ui) {
-      $("#amount").val(ui.values[0]+"€ - " + ui.values[1]+"€");
+      $("#amount").val(ui.values[0] + "€ - " + ui.values[1] + "€");
     }
   });
   $("#amount").val(
@@ -15,3 +15,20 @@ $(function() {
       $("#slider-range").slider("values", 1)
   );
 });
+
+function show_categories(this_) {
+  var categories = document.getElementById("categories");
+
+  if (categories.style.display == "none") {
+    categories.style.display = "inline-block";
+  } else {
+    categories.style.display = "none";
+  }
+}
+
+function change_category(category) {
+  var categories = document.getElementById("categories");
+  var label_category = document.getElementById("label_category");
+  label_category.innerHTML = category;
+  categories.style.display = "none";
+}
