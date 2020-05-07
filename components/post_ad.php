a02e306037ba2c6623b0f801ab7ea494829b5c96
StephaneBranly <stephanebranly.pro@gmail.com>
2020-05-07 20:07:51 +0200

    add: form post_ad

--- components/post_ad.php
+++ components/post_ad.php
@@ -1,6 +1,27 @@
 <?php
     function post_ad()
     {
+        echo "<section id='post_ad'>
+        <form method='post'>
+            <h1><input placeholder='Titre annonce' type='text'/></h1>
+            <h2>Photos</h2>
+            <input type='file'/>
+            <input type='file'/>
+            <input type='file'/>
 
+            <h2>Détails de l'annonce</h2>
+            <div class='an_input'><input type='number' min='0'/><i class='icon-euro'></i></div>
+            <div class='an_input'>
+                <select class='visibility'>
+                    <option>Tout le monde</option>
+                    <option>Utilisateur connecté</option>
+                </select>  <i class='icon-eye'></i>
+            </div>
+            <textarea placeholder='Description annonce'/></textarea>
+            
+          
+            <button type='submit' id='button_submit'>PUBLIER<i class='icon-paper-plane'></i></button>
+            </form>
+            </section>";
     }
 ?>
