a5d6231684ad6ec54326f0357f5ad9baf0f63383
StephaneBranly <stephanebranly.pro@gmail.com>
2020-06-27 18:33:33 +0200

    fix: header for prod

--- pages/login.php
+++ pages/login.php
@@ -3,6 +3,7 @@
 
 <!DOCTYPE html>
 <base href="http://localhost/LeBonCup/pages/"; />
+<!--<base href="https://assos.utc.fr/leboncup/pages/"; />-->
 <html>
 	<link href="../ressources/design/body.css" rel="stylesheet" media="all" type="text/css">
 	<link rel="icon" href="../ressources/images/favicon.ico" type="image/x-icon"/>
