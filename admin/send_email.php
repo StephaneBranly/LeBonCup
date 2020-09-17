2fc95d3c8e6b1db886e4d01a2a493a34e38d2946
StephaneBranly <stephanebranly.pro@gmail.com>
2020-09-17 20:02:47 +0200

    fix : validation button before sending emails

--- admin/send_email.php
+++ admin/send_email.php
@@ -1,8 +1,4 @@
-<?php
-    
-?> 
-
-<?php include_once("../lib/start_session.php");?>
+ <?php include_once("../lib/start_session.php");?>
 <!DOCTYPE html>
 <base href="http://localhost/LeBonCup/pages/"; />
 <!--<base href="https://assos.utc.fr/leboncup/pages/"; />-->
@@ -28,42 +24,48 @@
 
         if(!empty($_POST))
         {
-            $iduser = SQLProtect(secure_post('iduser'),1);
-
-            $subject = "LeBonCup - ".secure_post('title');
-
-            $headers = "From: leboncup@assos.utc.fr \r\n";
-            $headers .= "Reply-To: stephane.branly@etu.utc.fr \r\n";
-            $headers .= "MIME-Version: 1.0\r\n";
-            $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-            
-            $message=secure_post('content');
-            $message=$message."<br/><br/><br/><i>Merci de ne pas répondre à ce mail</i> | <a href='https://assos.utc.fr/leboncup'>LeBonCup</a>";
-
-            if($iduser=="tout_le_monde")
+            $validation=SQLProtect(secure_post('validation'),1);
+            if($validation)
             {
-                $query = mysqli_query($connect,"SELECT * FROM `users` ORDER BY `iduser` ASC");
-                while($res = mysqli_fetch_array($query))
+                $iduser = SQLProtect(secure_post('iduser'),1);
+
+                $subject = "LeBonCup - ".secure_post('title');
+
+                $headers = "From: leboncup@assos.utc.fr \r\n";
+                $headers .= "Reply-To: stephane.branly@etu.utc.fr \r\n";
+                $headers .= "MIME-Version: 1.0\r\n";
+                $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+                
+                $message=secure_post('content');
+                $message=$message."<br/><br/><br/><i>Merci de ne pas répondre à ce mail</i> | <a href='https://assos.utc.fr/leboncup'>LeBonCup</a>";
+
+                if($iduser=="tout_le_monde")
+                {
+                    $query = mysqli_query($connect,"SELECT * FROM `users` ORDER BY `iduser` ASC");
+                    while($res = mysqli_fetch_array($query))
+                        if($res['mail']!='')
+                        {
+                            $message_copy=$message;
+                            $message_copy=str_replace("[username]",$res['username'], $message_copy);
+                            mail($res['mail'], $subject, $message_copy, $headers);
+                        }
+                    echo "<script type='text/javascript'>write_notification('icon-paper-plane','Mail envoyé à tout le monde','5000');</script>";  
+                }
+                else
+                {
+                    $query = mysqli_query($connect,"SELECT * FROM `users` WHERE `iduser`='$iduser'");
+                    $res = mysqli_fetch_array($query);
                     if($res['mail']!='')
                     {
                         $message_copy=$message;
                         $message_copy=str_replace("[username]",$res['username'], $message_copy);
                         mail($res['mail'], $subject, $message_copy, $headers);
+                        echo "<script type='text/javascript'>write_notification('icon-paper-plane','Mail envoyé à $res[mail]','5000');</script>";
                     }
-                echo "<script type='text/javascript'>write_notification('icon-paper-plane','Mail envoyé à tout le monde','5000');</script>";  
-            }
-            else
-            {
-                $query = mysqli_query($connect,"SELECT * FROM `users` WHERE `iduser`='$iduser'");
-                $res = mysqli_fetch_array($query);
-                if($res['mail']!='')
-                {
-                    $message_copy=$message;
-                    $message_copy=str_replace("[username]",$res['username'], $message_copy);
-                    mail($res['mail'], $subject, $message_copy, $headers);
-                    echo "<script type='text/javascript'>write_notification('icon-paper-plane','Mail envoyé à $res[mail]','5000');</script>";
                 }
             }
+            else
+                echo "<script type='text/javascript'>write_notification('icon-exclamation','Il faut valider avant d envoyer un mail','5000');</script>";
         }
         echo"<section id='admin'>
         <h1>Envoie de mail</h1>
@@ -76,6 +78,7 @@
             echo"</select>
             <h2>Titre</h2><input type='text' name='title' maxlenght='190'/><br/>
             <h2>Contenu</h2><textarea type='text' name='content' maxlenght='1000'/></textarea><br/>
+            Validation : <input name='validation' type='checkbox'/>
             <button type='submit'>Envoyer mail<i class='icon-paper-plane'></i></button>
         </form>
         <a href='../admin/home'>Retour</a>
