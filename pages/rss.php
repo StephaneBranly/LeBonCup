fe1ff3298f952a60d6acda78e89ee87c37eadcab
Stephane_Branly <stephanebranly.pro@gmail.com>
2021-02-11 22:53:45 +0100

    fix: flux RSS

--- pages/rss.php
+++ pages/rss.php
@@ -3,6 +3,7 @@
     include_once("../lib/start_session.php");
     $query = mysqli_query($connect,"SELECT * FROM `ads` 
                 INNER JOIN `categories` ON ads.category = categories.category_cleaned
+                INNER JOIN `users` ON users.iduser = ads.seller
                 WHERE ads.status = 'to_sell' AND ads.visibility = 'every_one'
                 ORDER BY ads.last_refresh DESC");
  
@@ -18,12 +19,12 @@
     $myUrl = "https://assos.utc.fr/leboncup";
     while($res = mysqli_fetch_array($query))
     {
-        $link = $myUrl."/ad/-/".clean_string($res['title'])."-".$res['idad'];
+        $link = $myUrl."/ad/".$res['category_cleaned']."/".clean_string($res['title'])."-".$res['idad'];
         $imageUrl = $myUrl."/ressources/images-ad/".$res['image1'];
         echo "<item>
             <title>$res[title]</title>
             <id>$res[idad]</id>
-            <seller>$res[seller]</seller>
+            <seller>$res[username]</seller>
             <category>$res[category]</category>
             <price>$res[price]€></price>
             <last_refresh>$res[last_refresh]</last_refresh>
