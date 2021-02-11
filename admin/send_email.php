8a2ef00dce01a05bda037c33fef157391dc8eb08
Stephane_Branly <stephanebranly.pro@gmail.com>
2021-02-11 18:15:05 +0100

    fix: emails

--- admin/send_email.php
+++ admin/send_email.php
@@ -1,4 +1,5 @@
 <?php include_once("../lib/start_session.php");?>
+<?php include_once("./admin.php");?>
 <script type="text/javascript" src="./admin.js"></script>
 <?php include_once("../lib/document_base.php"); ?>
 <!DOCTYPE html>
@@ -36,26 +37,7 @@
             $headers .= "MIME-Version: 1.0\r\n";
             $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
             
-            $message="<div style=\"margin: 0px;
-            padding: 0px;
-            font-family: Arial, Helvetica, sans-serif;
-            text-align: center;
-            background-color: rgb(230, 230, 230);\" >
-            <div style=\"background-color: rgb(250, 250, 250);
-            width: 90%;
-            display: inline-block;
-            margin-bottom: 50px;
-            margin-top: 50px;
-            box-shadow: 5px 0px 10px rgb(221, 221, 221);
-            border-radius: 5px;\">
-            <h1 style=\"color: rgb(255, 98, 0);
-            border-bottom: 1px solid rgb(221, 221, 221);
-            width: 90%;
-            padding-bottom: 5px;
-            display: inline-block;\">LeBonCup</h1>
-            <p>".secure_post('content')."
-            <p><br/><p><i>Merci de ne pas répondre à ce mail</i> | <a href='https://assos.utc.fr/leboncup'>LeBonCup</a> | <a href='https://assos.utc.fr/leboncup/unsubscribe/[iduser]/news/[code]'>Se désabonner de la mailing list News</a></p></div></div>";
-
+            $message=generate_news_email(secure_post('content'));
                 if($iduser=="tout_le_monde")
                 {
                     $query = mysqli_query($connect,"SELECT * FROM `users` ORDER BY `iduser` ASC");
